dcf9c8fc03a00c44538f2a49a5cd01caf63b6fbf
A-PC\Андрей <belousovandrey3105@gmail.com>
2023-04-03 20:50:29 +0300

    start

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,17 +1,14 @@
-import time
 
 import allure
-import requests
-from selenium.webdriver import Keys
 
 from locators.main_page_locators import MainPageLocators
+from pages.base_page import BasePage
 
 
-
-class MainPage(BasePage):
-    locators = MainPageLocators()
-
-    @allure.step('regions_cookies')
-    def check_regions_cookies(self):
-        self.element_is_visible(self.locators.REGIONS_BUTTON).click()
-        self.element_is_visible(self.locators.COOKIE_BUTTON).click()
+# class MainPage(BasePage):
+#     locators = MainPageLocators()
+#
+#     @allure.step('regions_cookies')
+#     def check_regions_cookies(self):
+#         self.element_is_visible(self.locators.REGIONS_BUTTON).click()
+#         self.element_is_visible(self.locators.COOKIE_BUTTON).click()
